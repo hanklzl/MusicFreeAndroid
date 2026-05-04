3142219cb3dec97b64400182002965567925677c
zili <lzl0120@gmail.com>
2026-05-04 12:52:32 +0800

    chore: give debug apk a separate package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
