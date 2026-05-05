fb4db9e340c69156e18a09d3aa5d69903ebc1b87
zili <lzl0120@gmail.com>
2026-05-05 11:27:57 +0800

    feat(logging): add structured logging facade

---
+++ logging/src/main/java/com/zili/android/musicfreeandroid/logging/LogCategory.kt
@@ -0,0 +1,10 @@
+package com.zili.android.musicfreeandroid.logging
+
+enum class LogCategory(val wireName: String) {
+    APP("app"),
+    PLUGIN("plugin"),
+    SEARCH("search"),
+    PLAYER("player"),
+    PLAYLIST_IMPORT("playlist_import"),
+    FEEDBACK("feedback"),
+}
